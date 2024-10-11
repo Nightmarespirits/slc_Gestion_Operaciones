20805055079a481408a2cb2bfeed34a288a6fdfd
ns <clidertutayarivera@gmail.com>
2024-10-11 12:17:37 -0500

    router modificado

--- src/router/index.js
+++ src/router/index.js
@@ -12,7 +12,7 @@ const router = createRouter({
         {
             path: '/login',
             name: 'Login',
-            component: () => import('../login.vue'),
+            component: () => import('../Login.vue'),
             meta:{requiresAuth: false}
         },
         {
