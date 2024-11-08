f46903e573bef99a50328fe63852dc4f5ddf61ed
ns <clidertutayarivera@gmail.com>
2024-11-07 22:18:54 -0500

    Jueves 07 de Noviembre - correccion

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import { useAuthStore } from "../store/auth";
-import AppRoutes from "./appRoutes";
+import AppRoutes from "./AppRoutes";
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
